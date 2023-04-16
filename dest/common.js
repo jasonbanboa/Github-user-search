5162fc39d05e89038021ad22985d4cb660281630
jasonbanboa <jasonbanboa123@gmail.com>
2023-04-16 13:54:09 +0900

    fetching userdata

--- dest/common.js
+++ dest/common.js
@@ -1,3 +1,36 @@
 "use strict";
-const a = [1, 2, 3, 4, 5];
-a.forEach(el => console.log(el + 's'));
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function fetchUserData(username) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const { avatar_url, name, login, created_at, bio, public_repos, followers, following, location, twitter_username, blog, company, } = yield fetch(`https://api.github.com/users/${username}`).then(res => res.json());
+        return {
+            avatar_url,
+            name,
+            login,
+            created_at,
+            bio,
+            public_repos,
+            followers,
+            following,
+            location,
+            twitter_username,
+            blog,
+            company,
+        };
+    });
+}
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const userData = yield fetchUserData('jasonbanboa');
+        console.log(userData);
+    });
+}
+main();
--- src/common.ts
+++ src/common.ts
@@ -1,4 +1,56 @@
 
-const a = [1,2,3,4,5];
+interface UserData {
+  avatar_url: string,
+  name: string,
+  login: string,
+  created_at: string,
+  bio: string | null,
 
-a.forEach(el => console.log(el + 's'))
+  public_repos: number,
+  followers: number,
+  following: number,
+
+  location: string, 
+  twitter_username: string | null,
+  blog: string | null,
+  company: string | null,
+}
+
+async function fetchUserData(username: string): Promise<UserData> {
+  const {
+    avatar_url,
+    name,
+    login,
+    created_at,
+    bio,
+    public_repos,
+    followers,
+    following,
+    location,
+    twitter_username,
+    blog,
+    company,
+  } = await fetch(`https://api.github.com/users/${username}`).then(res => res.json());
+
+  return {
+    avatar_url,
+    name,
+    login,
+    created_at,
+    bio,
+    public_repos,
+    followers,
+    following,
+    location,
+    twitter_username,
+    blog,
+    company,
+  };
+}
+
+async function main() {
+  const userData: UserData = await fetchUserData('jasonbanboa');
+  console.log(userData);
+}
+
+main();
